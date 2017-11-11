c19b8ba09965242666dfdb156fb64352ea0bfd2c
kogayushi <youshe.old.pleasure@gmail.com>
2017-11-11 22:17:18 +0900

    page 4

---
+++ src/test/java/com/example/tdd/MoneyTest.java
@@ -0,0 +1,14 @@
+package com.example.tdd;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MoneyTest {
+    @Test
+    public void TestMultiplication() {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
+    }
+}
