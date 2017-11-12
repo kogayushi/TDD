ea5099dab705999bf0388fc2e169367a6f3c458b
kogayushi <youshe.old.pleasure@gmail.com>
2017-11-12 23:20:49 +0900

    fix typo

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertEquals;
 
 public class MoneyTest {
     @Test
-    public void TestMultiplication() {
+    public void testMultiplication() {
         Dollar five = new Dollar(5);
         Dollar product = five.times(2);
         assertEquals(10, product.amount);
