a7bad4c8b7a206e376911010c1a155e981c21582
kogayushi <youshe.old.pleasure@gmail.com>
2017-11-15 23:12:10 +0900

    page 31

---
+++ src/main/java/money/Franc.java
@@ -0,0 +1,19 @@
+package money;
+
+public class Franc {
+    private int amount;
+
+    public Franc(int amount) {
+        this.amount = amount;
+    }
+
+    public Franc times(int multiplier) {
+        return new Franc(this.amount * multiplier);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        Franc dollar = (Franc)object;
+        return amount == dollar.amount;
+    }
+}
