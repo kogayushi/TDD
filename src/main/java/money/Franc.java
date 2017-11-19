264b684afb9ffe34f8f340d347fe80f704341c3c
kogayushi <youshe.old.pleasure@gmail.com>
2017-11-19 23:03:04 +0900

    page 40 step 4

--- src/main/java/money/Franc.java
+++ src/main/java/money/Franc.java
@@ -11,7 +11,7 @@ public class Franc extends Money {
 
     @Override
     public boolean equals(Object object) {
-        Money dollar = (Money)object;
-        return amount == dollar.amount;
+        Money money = (Money)object;
+        return amount == money.amount;
     }
 }
