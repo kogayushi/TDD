9ad23a31aa3ba486476481f6c6eabc2038abbd43
kogayushi <youshe.old.pleasure@gmail.com>
2017-11-20 22:20:49 +0900

    page 58 step 2

--- src/main/java/money/Franc.java
+++ src/main/java/money/Franc.java
@@ -1,16 +1,11 @@
 package money;
 
 public class Franc extends Money {
-    private String currency;
     public Franc(int amount) {
         this.amount = amount;
         currency = "CHF";
     }
 
-    @Override
-    String currency() {
-        return currency;
-    }
 
     public Money times(int multiplier) {
         return new Franc(this.amount * multiplier);
