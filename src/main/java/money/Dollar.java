a03573e7c982a118e6324b6a1e0557815d7f3fcd
kogayushi <youshe.old.pleasure@gmail.com>
2017-11-20 22:04:57 +0900

    page 57

--- src/main/java/money/Dollar.java
+++ src/main/java/money/Dollar.java
@@ -1,14 +1,15 @@
 package money;
 
 public class Dollar extends Money {
-
+    private String currency;
     public Dollar(int amount) {
         this.amount = amount;
+        currency = "USD";
     }
 
     @Override
     String currency() {
-        return "USD";
+        return currency;
     }
 
     public Money times(int multiplier) {
