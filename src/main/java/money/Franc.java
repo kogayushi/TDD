be385207814eb0f6e1ccfd7ea30cb02a7afe1e7d
kogayushi <youshe.old.pleasure@gmail.com>
2017-11-26 20:43:32 +0900

    page 69

--- src/main/java/money/Franc.java
+++ src/main/java/money/Franc.java
@@ -6,6 +6,6 @@ public class Franc extends Money {
     }
 
     public Money times(int multiplier) {
-        return new Money(this.amount * multiplier, this.currency);
+        return new Franc(this.amount * multiplier, this.currency);
     }
 }
