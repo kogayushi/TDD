3bdd60bb0af950f931d48ae4204c1ee4dfad613b
kogayushi <youshe.old.pleasure@gmail.com>
2017-11-26 20:46:44 +0900

    page 70 step 2

--- src/main/java/money/Money.java
+++ src/main/java/money/Money.java
@@ -3,24 +3,29 @@ package money;
 class Money {
     protected int amount;
     protected String currency;
+
     Money(int amount, String currency) {
         this.amount = amount;
         this.currency = currency;
     }
+
     public Money times(int multiplier) {
         return new Money(this.amount * multiplier, this.currency);
     }
+
     String currency() {
         return currency;
     }
+
     public boolean equals(Object object) {
-        Money money = (Money)object;
-        return amount == money.amount && getClass().equals(money.getClass());
+        Money money = (Money) object;
+        return amount == money.amount && currency().equals(money.currency());
     }
 
     static Money dollar(int amount) {
         return new Dollar(amount, "USD");
     }
+
     static Franc franc(int amount) {
         return new Franc(amount, "CHF");
     }
