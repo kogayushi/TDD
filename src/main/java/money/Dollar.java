abda104ef706950803fcb8f2bbf1384985b170fd
kogayushi <youshe.old.pleasure@gmail.com>
2017-11-26 20:49:31 +0900

    page 71

--- src/main/java/money/Dollar.java
+++ src/main/java/money/Dollar.java
@@ -4,10 +4,4 @@ public class Dollar extends Money {
     public Dollar(int amount, String currency) {
         super(amount, currency);
     }
-
-    public Money times(int multiplier) {
-        return new Money(this.amount * multiplier,  this.currency);
-    }
-
-
 }
