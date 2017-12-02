a787efdef7fd66b3e65d31fe9be855dc0adb9d3d
kogayushi <youshe.old.pleasure@gmail.com>
2017-12-02 16:33:45 +0900

    page 118 step 2

--- src/main/java/money/Sum.java
+++ src/main/java/money/Sum.java
@@ -9,8 +9,14 @@ class Sum implements Expression {
         this.addend = addend;
     }
 
+    public Expression plus(Expression addend) {
+        return null;
+    }
+
     public Money reduce(Bank bank, String to) {
         int amount = augend.reduce(bank, to).amount + addend.reduce(bank, to).amount;
         return new Money(amount, to);
     }
+
+
 }
